fdf2a850d8e6bf85293aad1aac58b343c4296553
nakachan1994 <nakachaaaan@gmail.com>
2021-07-01 08:07:40 +0000

    [add]post機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3e08b0ceeba52a3f9930823526f293ada2b517cf785d3e89f5c15e244836a59596ff2750479537caddee75a87a85e0987647c530bb7a4cd1eb82d52daacd69e5'
