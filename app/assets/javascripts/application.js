9a8b47c98130a54a55d613f8b01de6c421c1c613
nakachan1994 <nakachaaaan@gmail.com>
2021-07-13 01:35:29 +0000

    [add]ローディング

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,3 +55,9 @@ $(document).on('turbolinks:load', function() {
     });
   });
 });
+
+// ローディング画面の
+$(window).on('load',function(){
+  $("#loading").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#loading_box").delay(1200).fadeOut('slow');//ローディングテキストを1.2秒（1200ms）待機してからフェードアウト
+});
