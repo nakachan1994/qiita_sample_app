feb63abad76a6152cd37b4e13e055fd82bff5d18
nakachan1994 <nakachaaaan@gmail.com>
2021-07-27 01:17:00 +0000

    [add]seedファイル

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create!(
+  content: "てすとほげ",
+  user_id: 1
+)
+
+# 一括で10個のユーザーデータを作成
+# 10回繰り返し処理を行う
+10.times do |n|
+  User.create!(
+    # n + 1で数字が重複しないようにする
+    email: "test#{n + 1}@test.com",
+    name: "テストユーザー#{n + 1}",
+    password: "testhoge"
+  )
+end
