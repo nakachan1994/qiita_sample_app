d7c1aa4dada6f9be9ef4a5ccbbef6f9bf34a091c
nakachan1994 <nakachaaaan@gmail.com>
2021-08-03 01:27:57 +0000

    [add]ransackでの検索機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  before_action :search
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def search
+    # params[:q]のqには検索フォームに入力した値が入る
+    @q = User.ransack(params[:q])
+  end
+
+  def index
+    # distinct: trueは重複したデータを除外
+   @users = @q.result(distinct: true)
   end
 
   def edit
