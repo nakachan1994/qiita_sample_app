fd37a5a5ff09baa3277de7d35b25d9d69a5acd6e
nakachan1994 <nakachaaaan@gmail.com>
2021-08-04 02:37:02 +0000

    [add]user/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を変更しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
